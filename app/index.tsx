80d36185cca602fab6ec44656bf8a00bc6fff910
Abayomiokojie <Abayomiokojie@gmail.com>
2025-09-09 03:43:38 -0400

    expo and tailwind setup

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { StatusBar, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Aora.</Text>
+      <StatusBar barStyle="dark-content" backgroundColor="black" />
+      <Link href="/profile" className="text-blue-500 text-5xl font-bold">
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
---
+++ app/profile.tsx
@@ -0,0 +1,18 @@
+import {
+  // StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+
+const profile = () => {
+  return (
+    <View>
+      <Text className="text-3xl font-bold">profile</Text>
+    </View>
+  );
+};
+
+export default profile;
+
+// const styles = StyleSheet.create({});
